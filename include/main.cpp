6ba7bf0ab85df9bfbcf43e01e5a47a8a10636c04
Fasih Ahmed <fasihahmed10513@gmail.com>
2025-12-07 13:30:22 +0500

    Enhanced version of main.cpp to handle additional features
    Refactor main function to utilize SocialNetwork class and remove unused functions.

--- include/main.cpp
+++ include/main.cpp
@@ -1,119 +1,73 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <map>
-#include "Graph.h"
+#include "SocialNetwork.h"
 using namespace std;
-
-Graph g;
-map<int, string> userNames;
-map<int, string> userCities;
-
-void loadNames(const string &filename)
-{
-    ifstream file(filename);
-    if (!file)
-    {
-        cerr << "Cannot open names file\n";
-        return;
-    }
-
-    string line;
-    while (getline(file, line))
-    {
-        if (line.empty())
-            continue;
-        int id;
-        char colon;
-        string name;
-        stringstream ss(line);
-        ss >> id >> colon;
-        getline(ss, name);
-        userNames[id] = name;
-    }
-}
-
-void loadCities(const string &filename)
-{
-    ifstream file(filename);
-    if (!file)
-    {
-        cerr << "Cannot open cities file\n";
-        return;
-    }
-
-    string line;
-    while (getline(file, line))
-    {
-        if (line.empty())
-            continue;
-        int id;
-        char colon;
-        string city;
-        stringstream ss(line);
-        ss >> id >> colon;
-        getline(ss, city);
-        userCities[id] = city;
-    }
-}
-
-void loadFriendshipData(const string &filename)
-{
-    ifstream file(filename);
-    if (!file)
-    {
-        cerr << "Cannot open friendship file\n";
-        return;
-    }
-
-    string line;
-    while (getline(file, line))
-    {
-        if (line.empty())
-            continue;
-
-        stringstream ss(line);
-        int user;
-        char colon;
-        ss >> user >> colon;
-
-        string name = userNames.count(user) ? userNames[user] : "User" + to_string(user);
-        string city = userCities.count(user) ? userCities[user] : "";
-
-        g.addVertex(user, name, city);
-
-        int friendID;
-        while (ss >> friendID)
-        {
-            string friendName = userNames.count(friendID) ? userNames[friendID] : "User" + to_string(friendID);
-            string friendCity = userCities.count(friendID) ? userCities[friendID] : "";
-            g.addVertex(friendID, friendName, friendCity);
-            g.addEdge(user, friendID);
-        }
-    }
-}
 
 int main()
 {
-    // Load data
-    loadNames("names.txt");
-    loadCities("cities.txt");
-    loadFriendshipData("friends.txt");
+    SocialNetwork sn;
+    sn.loadData();
 
-    map<string, string> cityColors = {
-        {"Karachi", "lightblue"}, {"Lahore", "lightgreen"}, {"Islamabad", "lightpink"}, {"Rawalpindi", "yellow"}, {"Faisalabad", "orange"}, {"Multan", "violet"}, {"Hyderabad", "cyan"}, {"Gujranwala", "gold"}, {"Sialkot", "magenta"}, {"Quetta", "salmon"}};
+    cout<<"==== Social Network Loaded ====\n";
 
-    g.exportToGraphviz("graph.dot", cityColors);
+    int choice;
+    while(true)
+    {
+        cout<<"\n1: Show User Info\n2: Add Post\n3: Like Post\n4: Comment Post\n5: Add Friend\n6: Shortest Path\n7: Mutual Friends\n0: Exit\nChoice: ";
+        cin>>choice;
 
-    // Mutual friends
-    vector<int> m = g.mutualFriends(1, 2);
-    cout << "Mutual friends of 1 and 2:\n";
-    for (int x : m)
-        cout << x << " ";
-    cout << endl;
+        if(choice==0) break;
 
-    // DFS path test
-    g.DFS(1, 10);
+        int uid,fid,pidx;
+        string content;
+        vector<int> path;
+        switch(choice)
+        {
+            case 1:
+                cout<<"UserID: "; cin>>uid;
+                sn.printUserInfo(uid);
+                break;
+            case 2:
+                cout<<"UserID: "; cin>>uid;
+                cin.ignore();
+                cout<<"Content: "; getline(cin,content);
+                sn.addPost(uid,content);
+                break;
+            case 3:
+                cout<<"Your ID: "; cin>>uid;
+                cout<<"Friend ID: "; cin>>fid;
+                cout<<"Post Index (0-based): "; cin>>pidx;
+                sn.addLike(uid,fid,pidx);
+                break;
+            case 4:
+                cout<<"Your ID: "; cin>>uid;
+                cout<<"Friend ID: "; cin>>fid;
+                cout<<"Post Index (0-based): "; cin>>pidx;
+                cin.ignore();
+                cout<<"Comment: "; getline(cin,content);
+                sn.addComment(uid,fid,pidx,content);
+                break;
+            case 5:
+                cout<<"Your ID: "; cin>>uid;
+                cout<<"Friend ID: "; cin>>fid;
+                sn.addFriend(uid,fid);
+                break;
+            case 6:
+                cout<<"User1 ID: "; cin>>uid;
+                cout<<"User2 ID: "; cin>>fid;
+                path = sn.g.shortestPath(uid,fid);
+                if(path.empty()) cout<<"No path found\n";
+                else {cout<<"Path: "; for(int x:path) cout<<x<<" "; cout<<endl;}
+                break;
+            case 7:
+                cout<<"User1 ID: "; cin>>uid;
+                cout<<"User2 ID: "; cin>>fid;
+                path = sn.g.mutualFriends(uid,fid);
+                cout<<"Mutual Friends: "; for(int x:path) cout<<x<<" "; cout<<endl;
+                break;
+            default: cout<<"Invalid choice\n";
+        }
+    }
 
     return 0;
 }
+
